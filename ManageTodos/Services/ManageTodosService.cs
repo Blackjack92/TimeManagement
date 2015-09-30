917ae436d81daca34822e7f48a830c203140bcc3
Kevin <kevin.wallis@hotmail.com>
2015-09-30 19:13:56 +0200

    Begin with saving todos in a xml.

--- ManageTodos/Services/ManageTodosService.cs
+++ ManageTodos/Services/ManageTodosService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Xml.Linq;
 using TimeManager.ManageTodos.Models;
+using TimeManager.ManageTodos.Properties;
 
 namespace TimeManager.ManageTodos.Services
 {
@@ -11,6 +14,31 @@ namespace TimeManager.ManageTodos.Services
 
         #region properties
         public ObservableCollection<Todo> Todos { get { return todos; } private set { todos = value; } }
+        #endregion
+
+        #region ctor
+        public ManageTodosService()
+        {
+            todos = new ObservableCollection<Todo>();
+        }
+        #endregion
+
+        #region methods
+        private void ReadTodos()
+        {
+            var document = XDocument.Parse(Resources.Todos);
+
+            //var newsData = document.Descendants("NewsItem")
+            //    .GroupBy(x => x.Attribute("TickerSymbol").Value,
+            //    x => new NewsArticle
+            //    {
+            //        PublishedDate = DateTime.Parse(x.Attribute("PublishedDate").Value, CultureInfo.InvariantCulture),
+            //        Title = x.Element("Title").Value,
+            //        Body = x.Element("Body").Value,
+            //        IconUri = x.Attribute("IconUri") != null ? x.Attribute("IconUri").Value : null
+            //    })
+            //    .ToDictionary(group => group.Key, group => group.ToList());
+        }
         #endregion
     }
 }
