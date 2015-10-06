247a064ed40bcd5360099101918547d6933b4aa1
Kevin <kevin.wallis@hotmail.com>
2015-10-06 22:22:20 +0200

    Added fixed ribbon stuff.

--- TimeManager/Bootstrapper.cs
+++ TimeManager/Bootstrapper.cs
@@ -6,6 +6,11 @@ using TimeManager.Shell.Views;
 using Prism.Unity;
 using Microsoft.Practices.Unity;
 using TimeManager.Navigation;
+using Prism.Regions;
+using Microsoft.Practices.ServiceLocation;
+using Prism.RibbonRegionAdapter;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 
 namespace TimeManager
 {
@@ -20,6 +25,16 @@ namespace TimeManager
         protected override void InitializeShell()
         {
             Application.Current.MainWindow.Show();
+        }
+
+        protected override RegionAdapterMappings ConfigureRegionAdapterMappings()
+        {
+            var mappings = base.ConfigureRegionAdapterMappings();
+
+            mappings.RegisterMapping(typeof(Ribbon), ServiceLocator.Current.GetInstance<RibbonRegionAdapter>());
+            mappings.RegisterMapping(typeof(ContextMenu), ServiceLocator.Current.GetInstance<MergingItemsControlRegionAdapter>());
+
+            return mappings;
         }
 
         protected override void ConfigureModuleCatalog()
