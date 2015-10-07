7b4718145dbf1f6f9f51ebff91409802afdad4e2
Kevin <kevin.wallis@hotmail.com>
2015-10-07 21:29:03 +0200

    Added some stuff for the ribbon but it is still not working correct.

--- ManageProjects/Views/RibbonView.xaml.cs
+++ ManageProjects/Views/RibbonView.xaml.cs
@@ -1,13 +1,38 @@
-﻿namespace TimeManager.ManageProjects.Views
+﻿using Prism;
+using System;
+
+namespace TimeManager.ManageProjects.Views
 {
     /// <summary>
     /// Interaktionslogik für RibbonView.xaml
     /// </summary>
-    public partial class RibbonView
+    public partial class RibbonView : IActiveAware
     {
+        public event EventHandler IsActiveChanged;
+        private bool isActive;
+
         public RibbonView()
         {
             InitializeComponent();
+        }
+
+        public bool IsActive
+        {
+            get { return isActive; }
+            set
+            {
+                isActive = value;
+                if (isActive)
+                {
+                    UpdateRibbonSelection();
+                }
+            }
+        }
+
+        private void UpdateRibbonSelection()
+        {
+            var r = ribbon;
+            //ribbon.SelectedItem = ribbon.Items.Count > 0 ? ribbon.Items[0] : null;
         }
     }
 }
--- ManageTodos/Views/RibbonView.xaml.cs
+++ ManageTodos/Views/RibbonView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PostSharp.Patterns.Model;
 
 namespace TimeManager.ManageTodos.Views
 {
     /// <summary>
     /// Interaktionslogik für RibbonView.xaml
     /// </summary>
-    public partial class RibbonView
+    /// 
+    public partial class RibbonView : IActiveAware
     {
+        public event EventHandler IsActiveChanged;
+        private bool isActive;
+
         public RibbonView()
         {
             InitializeComponent();
+        }
+
+        public bool IsActive
+        {
+            get { return isActive; }
+            set {
+                isActive = value;
+                if (isActive)
+                {
+                    UpdateRibbonSelection();
+                }
+            }
+        }
+
+        private void UpdateRibbonSelection()
+        {
+            //if (ribbon.Items.Count > 0)
+            //{
+            //    //ribbon.
+            //    ribbon.InitializeComponent();
+            //    ribbon.SelectedItem = ribbon.Items[0];
+            //    ((Crystalbyte.UI.RibbonTab)ribbon.Items[0]).IsSelected = true;
+            //    ((Crystalbyte.UI.RibbonTab)ribbon.Items[0]).UpdateLayout();
+            //    ((Crystalbyte.UI.RibbonTab)ribbon.Items[0]).ApplyTemplate();
+            //    ((Crystalbyte.UI.RibbonTab)ribbon.Items[0]).BeginInit();
+
+            //    UpdateDefaultStyle();
+                
+            }
+
         }
     }
 }
