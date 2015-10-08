061f7acf8e2495e27e084bc1ba84b16f7af0f2a3
Kevin <kevin.wallis@hotmail.com>
2015-10-08 15:13:49 +0200

    Added images for delete and add.

--- ManageTodos/Properties/Resources.Designer.cs
+++ ManageTodos/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace TimeManager.ManageTodos.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace TimeManager.ManageTodos.Properties {
         ///   Gibt die zwischengespeicherte ResourceManager-Instanz zurück, die von dieser Klasse verwendet wird.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TimeManager.ManageTodos.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,95 @@ namespace TimeManager.ManageTodos.Properties {
         ///   Ressourcenzuordnungen, die diese stark typisierte Ressourcenklasse verwenden.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Add {
+            get {
+                object obj = ResourceManager.GetObject("Add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Add ähnelt.
+        /// </summary>
+        public static string AddTodo {
+            get {
+                return ResourceManager.GetString("AddTodo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Add ähnelt.
+        /// </summary>
+        public static string AddWorkingItem {
+            get {
+                return ResourceManager.GetString("AddWorkingItem", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Delete {
+            get {
+                object obj = ResourceManager.GetObject("Delete", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Edit ähnelt.
+        /// </summary>
+        public static string Edit {
+            get {
+                return ResourceManager.GetString("Edit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Manage Todos ähnelt.
+        /// </summary>
+        public static string ManageTodos {
+            get {
+                return ResourceManager.GetString("ManageTodos", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Manage Working Items ähnelt.
+        /// </summary>
+        public static string ManageWorkingItems {
+            get {
+                return ResourceManager.GetString("ManageWorkingItems", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Remove ähnelt.
+        /// </summary>
+        public static string RemoveTodo {
+            get {
+                return ResourceManager.GetString("RemoveTodo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Remove ähnelt.
+        /// </summary>
+        public static string RemoveWorkingItem {
+            get {
+                return ResourceManager.GetString("RemoveWorkingItem", resourceCulture);
             }
         }
         
@@ -75,16 +158,16 @@ namespace TimeManager.ManageTodos.Properties {
         ///    &lt;Title&gt;Second todo&lt;/Title&gt;
         ///    &lt;Description&gt;Second description&lt;/Description&gt; [Rest der Zeichenfolge wurde abgeschnitten]&quot;; ähnelt.
         /// </summary>
-        internal static string Todos {
+        public static string Todos {
             get {
                 return ResourceManager.GetString("Todos", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;        ///&lt;WorkingItems xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot;&gt;        ///  &lt;WorkingItem&gt;        ///    &lt;Id&gt;C2565AEC-9315-4008-80B9-962475E3FFCE&lt;/Id&gt;        ///    &lt;WorkingTimes&gt;        ///      &lt;WorkingTime Start=&quot;5/5/2015&quot; End=&quot;5/5/2015&quot; /&gt;        ///      &lt;!--&lt;WorkingTime Start=&quot;5/5/2015-12:12:12&quot; End=&quot;5/5/2015-12:22:12&quot; /&gt;--&gt;        ///    &lt;/WorkingTimes&gt;        ///  &lt;/WorkingItem&gt;        ///&lt;/WorkingItems&gt; ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;        ///&lt;WorkingItems xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot;&gt;        ///  &lt;WorkingItem&gt;        ///    &lt;Id&gt;C2565AEC-9315-4008-80B9-962475E3FFCE&lt;/Id&gt;        ///    &lt;Start&gt;5/5/2015&lt;/Start&gt;        ///    &lt;End&gt;5/6/2015&lt;/End&gt;        ///    &lt;Description&gt;Working item description&lt;/Description&gt;        ///  &lt;/WorkingItem&gt;        ///&lt;/WorkingItems&gt; ähnelt.
         /// </summary>
-        internal static string WorkingItems {
+        public static string WorkingItems {
             get {
                 return ResourceManager.GetString("WorkingItems", resourceCulture);
             }
---
+++ ManageTodos/Services/ResourceService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using TimeManager.ManageTodos.Properties;
+
+namespace TimeManager.ManageTodos.Services
+{
+    public static class ResourceService
+    {
+        public static ImageSource AddImageSource
+        {
+            get
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                          Resources.Add.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+        public static ImageSource RemoveImageSource
+        {
+            get
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                          Resources.Delete.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+    }
+}
