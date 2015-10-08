d218aaec52e6fb7dc9a9977f5262db4f672f8d1b
Kevin <kevin.wallis@hotmail.com>
2015-10-08 22:24:54 +0200

    Some more styling.

--- ManageTodos/Views/MainView.xaml.cs
+++ ManageTodos/Views/MainView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Data;
+using TimeManager.Infrastructure;
 
 namespace TimeManager.ManageTodos.Views
 {
@@ -7,6 +9,18 @@ namespace TimeManager.ManageTodos.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private void workingItemsDataGridTargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            todosDataGrid.UpdateLayout();
+            var workingItemsDataGrid = UIHelper.FindChild<DataGrid>(todosDataGrid, "workingItemsDataGrid");
+            if (workingItemsDataGrid != null)
+            {
+                workingItemsDataGrid.Columns[2].Width = 0;
+                workingItemsDataGrid.UpdateLayout();
+                workingItemsDataGrid.Columns[2].Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+            }
         }
     }
 }
