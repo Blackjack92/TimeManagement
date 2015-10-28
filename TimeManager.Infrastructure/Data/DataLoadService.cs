1737d4d532e7a5800402ac617c7a1d149bb85867
Kevin <kevin.wallis@hotmail.com>
2015-10-28 23:03:59 +0100

    Added data load service to the infrastructure.

---
+++ TimeManager.Infrastructure/Data/DataLoadService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using PostSharp.Patterns.Model;
+using Microsoft.Win32;
+using System.Xml.Linq;
+
+namespace TimeManager.Infrastructure.Data
+{
+    [NotifyPropertyChanged]
+    public class DataLoadService
+    {
+        #region fields
+        private readonly IEnumerable<IStoreRoot> rootsToStore;
+        #endregion
+
+        #region ctor
+        public DataLoadService(IEnumerable<IStoreRoot> rootsToStore)
+        {
+            this.rootsToStore = rootsToStore;
+        }
+        #endregion
+
+        #region methods
+        public void Open()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                var file = dialog.FileName;
+                var document = XDocument.Load(file);
+
+                // Store all store roots
+                foreach (var root in rootsToStore)
+                {
+                    foreach (XElement element in document.Descendants(root.GetType().Name))
+                    {
+                        var storeClass = DataStoreHelper.FindFirstStoreClass(root.GetType());
+                        var createdObject = DataStoreHelper.CreateObject(storeClass, new object[] { element });
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
