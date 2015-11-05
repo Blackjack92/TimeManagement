e52b3d5a9b5476b886962703c444250aea1648f8
Kevin <kevin.wallis@hotmail.com>
2015-11-05 16:53:56 +0100

    Added navigation styling.

---
+++ TimeManager.Navigation/Services/ResourceService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using TimeManager.Navigation.Properties;
+
+namespace TimeManager.Navigation.Services
+{
+    public static class ResourceService
+    {
+        public static ImageSource TodosImageSource
+        {
+            get
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                          Resources.Todos.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+    }
+}
